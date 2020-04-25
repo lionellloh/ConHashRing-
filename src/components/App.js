a66866f09e149257e08b2b65183308d5f85e5183
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-04-25 13:09:55 +0800

    initial ring

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Ring from "./Ring.js"
+import ApexChart from "./ApexChart";
+
+class App extends React.Component {
+
+  render() {
+
+    return (<div>
+      Hello from App.js
+      <ApexChart/>
+
+    </div>)
+  }
+}
+export default App;
