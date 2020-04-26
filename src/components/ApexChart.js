5caa3bad55f06051a0bddf3bb33d4ac578273ac4
Lionell <lionell_loh@mymail.sutd.edu.sg>
2020-04-27 01:41:35 +0800

    removed unecessary parts

--- src/components/ApexChart.js
+++ src/components/ApexChart.js
@@ -101,44 +101,8 @@ class ApexChart extends React.Component {
         )
     }
 
-    appendData() {
-        var arr = this.state.series.slice()
-        arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
-
-        this.setState({
-            series: arr
-        })
-    }
-
-    removeData() {
-        if (this.state.series.length === 1) return
-
-        var arr = this.state.series.slice()
-        arr.pop()
-
-        this.setState({
-            series: arr
-        })
-    }
-
-    randomize() {
-        this.setState({
-            series: this.state.series.map(function () {
-                return Math.floor(Math.random() * (100 - 1 + 1)) + 1
-            })
-        })
-    }
-
-    // reset() {
-    //     this.setState({
-    //         series: [44, 55, 13, 33]
-    //     })
-    // }
-
-
     render() {
         return (
-
 
             <div className="ui segment">
                 <div class="chart-wrap">
